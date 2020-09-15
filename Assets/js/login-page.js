5ee29aaa9d2605c5adb1bdf7329cba54a7f0a475
Pratik Dhungel <dhungel.1996@gmail.com>
2020-09-15 09:23:33 +0545

    Intergrate API to login form using Axios

--- Assets/js/login-page.js
+++ Assets/js/login-page.js
@@ -1,12 +1,24 @@
 const loginForm = document.getElementById('login-form');
 
-axios
-  .get('http://localhost:5000/api/v1/user/getUsers')
-  .then(function (response) {
-    // handle success
-    console.log(response);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  });
+// console.log('Hello World');
+loginForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  //   const formData = new FormData(loginForm);
+  const userEmail = document.getElementById('login-form-email-input').value;
+  const userPassword = document.getElementById('login-form-password-input')
+    .value;
+
+  userData = { email: userEmail, password: userPassword };
+
+  axios
+    .post('http://localhost:5000/api/v1/user/login', userData)
+    .then(function (response) {
+      // handle success
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      // handle error
+      alert(error.response.data.error);
+    });
+});
